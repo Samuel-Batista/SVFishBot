614e980b00d15ff5a54b9ecf460404ce60980815
Samuel-Batista <84580161+Samuel-Batista@users.noreply.github.com>
2022-07-12 18:12:00 -0300

    update

--- bot.py
+++ bot.py
@@ -1,17 +1,35 @@
-from anyio import current_time
-from numpy import imag, true_divide
 from window_capture import WindowCapture
+import numpy as np
 import cv2 as cv
-
+import pyautogui
+from time import sleep
 
 
 from PIL import Image
 cap = WindowCapture("Stardew Valley")
 
 
-while True:
-    current_image = cap.get_screenshot()
-    current_image.reshape(300, 300)
-    image = current_image.resize((500,500),Image.ANTIALIAS)
-    cv.imshow('game', current_image)
-    cv.waitKey(1)
+game_resolution = (1280, 721)
+
+def trow():
+    x, y = cap.get_screen_position(((game_resolution[0]//2)-300, game_resolution[1]//2))
+    # pyautogui.mouseDown(x, y)
+
+    while True:
+        game_screenshoot = cap.get_screenshot()
+        y_max = (game_resolution[0] // 2) - 420
+        y_min = (game_resolution[1] // 2) - 160
+        x_max = (game_resolution[0] // 2) + 98
+        x_min = (game_resolution[1] // 2) + 220
+        
+        trow_bar = game_screenshoot[y_min:y_max, x_min:x_max]
+        cv.imshow("game", trow_bar)
+        cv.waitKey(1)
+
+
+    
+    pyautogui.mouseUp()
+    # sleep(3)
+
+
+trow()
