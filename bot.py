41a6343d5b33a8b84d40ca75ceb3a31b706ac4e0
Samuel-Batista <84580161+Samuel-Batista@users.noreply.github.com>
2022-07-12 10:09:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,15 @@ from numpy import imag, true_divide
 from window_capture import WindowCapture
 import cv2 as cv
 
+
+
+from PIL import Image
 cap = WindowCapture("Stardew Valley")
 
 
 while True:
     current_image = cap.get_screenshot()
     current_image.reshape(300, 300)
+    image = current_image.resize((500,500),Image.ANTIALIAS)
     cv.imshow('game', current_image)
     cv.waitKey(1)
