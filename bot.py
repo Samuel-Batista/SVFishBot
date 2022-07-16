0f223fad282b08347768df860617f3da5befac91
Samuel-Batista <84580161+Samuel-Batista@users.noreply.github.com>
2022-07-15 23:40:14 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+from re import A
+from tkinter import CENTER
 from turtle import width
+
+from cv2 import FarnebackOpticalFlow
 from window_capture import WindowCapture
 import cv2 as cv
 import pyautogui
@@ -69,15 +73,22 @@ def wait_for_fish():
         cv.waitKey(1)
 
 
+
+
 def cath_fish():
-    # x, y = cap.get_screen_position(((game_resolution[0]//2), game_resolution[1]//2))
-    # pyautogui.mouseDown(x, y)
-    # sleep(0.3)
-    # pyautogui.mouseUp(x, y)
+    x, y = cap.get_screen_position(((game_resolution[0]//2), game_resolution[1]//2))
+    pyautogui.mouseDown(x, y)
+    sleep(0.3)
+    pyautogui.mouseUp(x, y)
 
     game_screenshot = cap.get_screenshot()
     screen_height, screen_width, _ = game_screenshot.shape
     screen_center_x, screen_center_y = screen_width // 2, screen_height // 2
+
+    top_pos = 0
+    botton_pos = 0
+    center_bar_pos = 0
+    fish_pos = 0
 
     while True:
         game_screenshot = cap.get_screenshot()
@@ -87,34 +98,83 @@ def cath_fish():
 
         # cv.rectangle(cath_bar, (bar_center_x - 30, bar_center_y - 5), (bar_center_x - 25, bar_center_y - 2), (0, 0, 255), 1)
         bar_spot = cath_bar[bar_center_y-5:bar_center_y-4, bar_center_x-30:bar_center_x - 29]
-
-        # [ 78 174 232]
+        # print(bar_spot)
+        # [ 67 142 203] bar
+        # [ 78 134 146] none
         b_bs, g_bs, r_bs = bar_spot[0][0]
-        if b_bs == 78 and g_bs == 174 and r_bs == 232:
+        if r_bs >= 200:
             # peixe e um sequencia de verdes e azuis
             # barra de pesca e uma sequencia igual de tons de verde
             # fundo sequencia variada de tons de azuis
 
+            
             # encontrar a parte de cima da barra de pesca
             for h in range(cath_bar_height):
-                b_p, g_p, r_p  = cath_bar[h:h+1, bar_center_x:bar_center_x+1][0][0]
+                b_p, g_p, r_p  = cath_bar[h:h+1, bar_center_x-15:bar_center_x-14][0][0]
 
                 if b_p < 200:
                     count = 0
 
-                    # verify 10 sequence
+                    # verify similary sequence of pixel
                     if h < cath_bar_height - 10:
-                        for plus in range(10):
-                            b_pp, g_pp, r_pp  = cath_bar[h+plus:(h+1)+plus, bar_center_x:bar_center_x+1][0][0]
+                        for plus in range(3):
+                            b_pp, g_pp, r_pp  = cath_bar[h+plus:(h+1)+plus, bar_center_x-15:bar_center_x-14][0][0]
                             if b_pp == b_p and g_pp == g_p and r_pp == r_p:
                                 count += 1
                     
-                    if count >= 10:
-                        cv.line(cath_bar, (bar_center_x - 50, h-2), (bar_center_x + 50, h-2), (0, 0, 255), 2)
+                    if count >= 3:
+                        cv.line(cath_bar, (bar_center_x - 50, h-2), (bar_center_x + 50, h-2), (255, 0, 0), 3)
+                        top_pos = h
                         break
-    
-                
 
+
+            # encontrar a parte de baixo da barra de pesca
+            for h2 in range(cath_bar_height-20, 1, -1):
+                b_p, g_p, r_p  = cath_bar[h2-1:h2, bar_center_x-15:bar_center_x-14][0][0]
+
+                if b_p < 200:
+                    count = 0
+
+                    # verify similary sequence of pixel
+                    if h2 > 10:
+                        for plus in range(1, 4):
+                            b_pp, g_pp, r_pp  = cath_bar[h2-plus:(h2-plus)+1, bar_center_x-15:bar_center_x-14][0][0]
+                            if b_pp == b_p and g_pp == g_p and r_pp == r_p:
+                                count += 1
+                    
+                    if count >= 3:
+                        botton_pos = h2
+                        break
+
+            # econtrar peixe
+            for hp in range(cath_bar_height):
+                b_p, g_p, r_p  = cath_bar[hp:hp+1, bar_center_x-15:bar_center_x-14][0][0]
+
+
+                if b_p < 70 and g_p < 70 and r_p < 70:
+                    fish_pos = hp + 15
+                    break
+
+            # encontrar centro da barra verde
+            midle_gap = int((botton_pos - top_pos) / 2)
+            center_bar_pos = top_pos + midle_gap
+            
+            # controll green bar
+            
+
+            # draw lines for debug
+            # top green bar
+            cv.line(cath_bar, (bar_center_x - 50, top_pos), (bar_center_x + 50, top_pos), (255, 0, 0), 3)
+
+            # botton green bar
+            cv.line(cath_bar, (bar_center_x - 50, botton_pos), (bar_center_x + 50, botton_pos), (255, 0, 0), 3)
+
+            # center green bar
+            cv.line(cath_bar, (bar_center_x - 50, center_bar_pos-4), (bar_center_x + 50, center_bar_pos-4), (255, 0, 255), 3)
+            cv.line(cath_bar, (bar_center_x - 50, center_bar_pos+4), (bar_center_x + 50, center_bar_pos+4), (255, 0, 255), 3)
+
+            # fish
+            cv.line(cath_bar, (bar_center_x - 50, fish_pos), (bar_center_x + 50, fish_pos), (0, 0, 255), 3)
 
 
             cv.imshow('cathBar', cath_bar)
